7a9b500c8d41f9fb85bf3978c9cac68dbd433d39
Berkant <berkantgurcan@hotmail.com>
2022-08-15 14:02:19 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
 import store from "./redux/store"
 
 
@@ -16,7 +15,3 @@ root.render(
   </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
